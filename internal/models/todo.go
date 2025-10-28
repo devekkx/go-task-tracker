79d05907c1f92ea2237b3c205eb79fb06be0f96d
Emmanuel Kpendo <devekkx@gmail.com>
2025-10-28 09:00:00 +0000

    feat(models): add TodoItem struct

--- internal/models/todo.go
+++ internal/models/todo.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+// TodoItem is a single item in a TodoList.
+type TodoItem struct {
+	ID        string    `json:"id"`
+	Content   string    `json:"content"`
+	Done      bool      `json:"done"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
