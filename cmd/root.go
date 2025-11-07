81527363cd29b4ec6c726237dd9050f6a908f0bf
Emmanuel Kpendo <devekkx@gmail.com>
2025-11-07 14:00:00 +0000

    style: consistent spacing

--- cmd/root.go
+++ cmd/root.go
@@ -15,7 +15,7 @@ var rootCmd = &cobra.Command{
 Your data is stored locally at ~/.task-tracker/data.json.`,
 }
 
-// Execute starts the root command
+// Execute launches the root command
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
