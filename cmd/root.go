e049b899cb09c491d19801cdf78042879a6141d1
Emmanuel Kpendo <devekkx@gmail.com>
2025-11-15 14:00:00 +0000

    style: group related code

--- cmd/root.go
+++ cmd/root.go
@@ -20,7 +20,7 @@ Examples:
   tracker todo create "Weekend Tasks"`,
 }
 
-// Execute launches the root command
+// Execute runs the root command
 // Execute runs the root command
 func Execute() {
 
