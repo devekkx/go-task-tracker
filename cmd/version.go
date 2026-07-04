dd747ff55273e545a42e383332b005038a463876
Emmanuel Kpendo <devekkx@gmail.com>
2026-07-04 17:10:00 +0000

    fix: move Version const after imports in version.go

--- cmd/version.go
+++ cmd/version.go
@@ -1,20 +1,18 @@
 package cmd
 
-// Version is the current application release.
-const Version = "1.1.0"
-
 import (
 	"fmt"
 
 	"github.com/spf13/cobra"
 )
 
-const version = "1.0.0"
+// Version is the current application release.
+const Version = "1.1.0"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print version information",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("tracker version %s\n", version)
+		fmt.Printf("tracker version %s\n", Version)
 	},
 }
